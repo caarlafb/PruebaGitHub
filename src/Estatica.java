dca257e9ea210743a92b844031b8f31a1357aa47
caarlafb <carladelafuente7@gmail.com>
2024-02-16 09:54:59 +0100

    Initial Comit for this proyect

---
+++ src/Estatica.java
@@ -0,0 +1,8 @@
+public class Estatica {
+    public static int contador;
+
+    public void incrementar(){
+        contador++;
+        System.out.println("el valor es: " + contador);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola de nuevo, mundo.");
+        Estatica estatica1 = new Estatica();
+        estatica1.incrementar();
+        Estatica estatica2 = new Estatica();
+        estatica2.incrementar();
+        Estatica estatica3 = new Estatica();
+        estatica3.incrementar();
     }
 }
